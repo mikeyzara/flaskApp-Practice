3532e45b88cf5b3056b88be94eef2caebda83af7
Michael Zara <mzara@ryerson.ca>
2022-10-05 21:29:05 -0400

    Updated Flask app

--- lab4-app.py
+++ lab4-app.py
@@ -14,30 +14,43 @@ def home():
 
 @app.route('/home/<int:num>', methods = ['GET'])
 def disp(num):
-	return jsonify({'data': num**2})
+	return jsonify({'data': num**2}) #return the squared value provided in the route
 
-@app.route('/test_json', methods = ['POST'])
-def test_json():
-	if(request.method == 'POST'):
-		#data_id = request.form.get('Id')
-		#data_customer = request.form.get('Customer')
-		#data_quantity  = request.form.get('Quantity')
-		#data_price = request.form.get('Price')
-		data_id = request.get_json()['Id']
-		data_customer = request.get_json()['Customer']
-		data_quantity = request.get_json()['Quantity']
-		data_price = request.get_json()['Price']
+@app.route('/test_json', methods = ['POST']) #Enable POST method for route
+def test_json():			     #We use RESTer to send a POST request
+	if(request.method == 'POST'): 		# Note: The POST request is a JSON object
+						#	We know this because we selected the
+						#	following in RESTer:
+						#	1. Content-type
+						#	2. application/json
+		
+		# Use the request.get_json() method to read/parse the POST request. get_json()
+		# returns a dictionary {'key':'value'}
+		data = request.get_json() #Returns a dictionary
+		#print(data)
+		
+		#Indexing the dictionary returned by request.json():
+		data_id = data['Id']  
+		data_customer = data['Customer']
+		data_quantity = data['Quantity']
+		data_price = data['Price']
+		
+		#Print parsed values into Terminal
 		print('Id: ', data_id)
 		print('Customer: ', data_customer)
 		print('Quantity: ', data_quantity)
 		print('Price: ', data_price)
 		return jsonify({'data': 'success'})
 		
-@app.route('/test', methods = ['GET','POST'])
-def test():
+@app.route('/test', methods = ['GET','POST']) #Enable GET and POST
+def test():					# We use this route for the webform
 	if(request.method == 'POST'):
-		content_type = request.headers.get('Content-Type')
-		print(content_type)
+		content_type = request.headers.get('Content-Type') #Check the Content-Type
+								   # from Header
+		print(content_type) #Print the content_type:
+				    # application/x-www-form-urlencoded
+		
+		# Use the request.form.get() method to parse the PSOT request from the webform
 		data_id = request.form.get('ID')
 		data_customer = request.form.get('Customer')
 		data_quantity  = request.form.get('Quantity')
@@ -46,7 +59,7 @@ def test():
 		print('Customer: ', data_customer)
 		print('Quantity: ', data_quantity)
 		print('Price: ', data_price)
-	return render_template('test.html')
+	return render_template('test.html') #Render test.html from templates directory
 
 if __name__ == '__main__':
 	app.run()
